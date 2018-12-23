c75e1084fd6996f0e2bddae5733461368b537d0c
dongY <ldcpaul@hanmail.net>
2018-12-23 13:24:58 +0900

    기본명명 , 콘솔로그 ,문자열 보간법

---
+++ Swift 기본문법/InIt.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+let age : Int = 26
+
+let name : String = "이동영"
+
+print("안녕하세요 저는 \(name)입니다. 나이는 \(age)살 입니다.")
+
+dump("내년에는 \(age+1)살입니다")
+
+class Person
+{
+    var name : String = "이동영"
+    var age : Int = 26
+}
+
+let 이동영 : Person = Person()
+
+dump(이동영)
+
+
+
+
