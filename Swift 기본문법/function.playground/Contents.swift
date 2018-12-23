8313215e08199e008fb4d4eb1f30c6edadd98f5f
dongY <ldcpaul@hanmail.net>
2018-12-23 17:53:34 +0900

    function  기본

---
+++ Swift 기본문법/function.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+
+func sum( a : Int , b: Int) -> Int{
+    return a+b
+}
+
+// 파리미터에 값만 넣으면 안된다 변수도 꼭 넣어줘야함
+print(sum(a:3,b:10))
+
+func multifly(a:Int , b:Int) ->Int{
+    return a*b;
+}
+
+print(multifly(a: 5, b: 10))
+
+
+func bye(){print("bye")}
+
+bye()
