8d4a5d341f0e22416de0469e9e8428a2a9e0db6c
dongY <ldcpaul@hanmail.net>
2018-12-23 13:40:40 +0900

    상수와 변수

---
+++ Swift 기본문법/var_let.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var variable : String = "변수는 변경이 가능합니다."
+let constance : String = "상수는 변경이 불가능 합니다."
+var t : String  = "선언과 동시에 초기화를 해주어야 합니다."
+let temp : String = "상수 또한 마찬가지 입니다"
+print(t)
+print(temp)
+
+
