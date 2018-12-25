7a05647149c5b6bbdf569d7221bd2f78e97c0f22
dongY <ldcpaul@hanmail.net>
2018-12-25 16:00:46 +0900

    옵셔널 , 옵셔널의 제약. : 크래시 방어

---
+++ Swift 기본문법/Optional.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+//옵셔널 변수선언
+// 옵셔널로 랩
+var optional1 : Int? = 3
+var optional2 : Optional <Int> = nil
+
+
+var normal : Int = 3
+
+//  normal = Optional1   --일반 변수에 대입불가능
+//   optional1+=1  optional 은 일반수식 불가능
+
+normal+=1
